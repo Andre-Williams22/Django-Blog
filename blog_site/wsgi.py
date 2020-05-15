226f4ef7b1aec5e4bf6845af24f88bd3dfc47b4a
Andre Williams <awjrs22@gmail.com>
2020-05-15 08:19:25 -0700

    fixes naming error

--- blog_site/wsgi.py
+++ blog_site/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 project_folder = os.path.expanduser('/Users/andre22/Documents/Dev/BEW-1.2/blog_site') 
-load_dotenv(os.path.join(project_folder, '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_site.settings')
 
 application = get_wsgi_application()
